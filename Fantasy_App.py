7b36c54d9e1a7670c64b64167b7b833f1a55480b
Jill Brubaker <jillbrubaker@Jills-MacBook-Air.local>
2022-12-02 20:21:22 +0100

    Sorting standings

--- Fantasy_App.py
+++ Fantasy_App.py
@@ -715,7 +715,7 @@ if authentication_status:
         info = {"name": ["Alex", "Tate", "Michael", "Mike", "Sander", "Fabian", "Gunnar", "Ronen", "Magnus", "Torje"],
                 "points": ["1700", "2336", "2173", "0", "0", "0", "1871", "0", "2736", "3061"]}
         df = pd.DataFrame(info)
-        st.dataframe(df.sort("points", ascending=False))
+        st.dataframe(df.sort_values(by="points", ascending=False))
 
 
 
