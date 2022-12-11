384c091ce45efa18726934eeef312ce1270a589a
Jill Brubaker <jillbrubaker@Jills-MacBook-Air.local>
2022-12-11 12:59:25 +0100

    Testing functions

--- Fantasy_App.py
+++ Fantasy_App.py
@@ -125,20 +125,18 @@ if authentication_status:
                 st.subheader("Cross Country")
                 gender_select = st.selectbox("Which gender?", genders)
                 if gender_select == "Men":
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        # TESTING
-                        def MEN_XC_ATHLETE_TABLE():
+                    def MEN_XC_ATHLETE_TABLE():
+                        col1, col2 = st.columns(2)
+                        with col1:
                             data = {'Athlete': male_XC_athletes, "Nationality": male_XC_nationality,
                                     "Points 21/22 Season": male_XC_points_2022, "Price": price_men_XC}
                             df = pd.DataFrame(data)
                             st.dataframe(df)
-                        MEN_XC_ATHLETE_TABLE()
-                        # TESTING
-                    with col2:
-                        selection = st.selectbox("Filter by nationality here", countries)
-                        Nationality_filter = df.loc[df['Nationality'] == f"{selection}"]
-                        st.dataframe(Nationality_filter)
+                        with col2:
+                            selection = st.selectbox("Filter by nationality here", countries)
+                            Nationality_filter = df.loc[df['Nationality'] == f"{selection}"]
+                            st.dataframe(Nationality_filter)
+                    MEN_XC_ATHLETE_TABLE()
                 if gender_select == "Women":
                     col1, col2 = st.columns(2)
                     with col1:
